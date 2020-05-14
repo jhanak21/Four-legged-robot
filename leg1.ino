4aadc247347f3a3d6ddcc7c3b1a79369e7690f97
jhini <45027200+jhini@users.noreply.github.com>
2020-05-14 10:09:48 +0530

    Add files via upload

---
+++ leg1.ino
@@ -0,0 +1,58 @@
+
+#include <Servo.h> 
+ Servo myservo1;  // create servo object to control a servo 
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;// twelve servo objects can be created on most boards
+ Servo myservo5;  // create servo object to control a servo 
+Servo myservo6;
+Servo myservo7;
+Servo myservo8;
+  // variable to store the servo position 
+  int d=1000;
+void setup() 
+{ 
+  myservo1.attach(2);
+  myservo2.attach(3);
+  myservo3.attach(4);
+  myservo4.attach(5);
+  myservo5.attach(9);
+  myservo6.attach(10);
+  myservo7.attach(11);
+   myservo8.attach(12);// attaches the servo on pin 9 to the servo object 
+
+
+  int pos = 40;  
+  myservo1.write(pos);
+  myservo2.write(pos);
+  myservo3.write(pos);
+  myservo4.write(pos);
+  myservo5.write(pos);
+  myservo6.write(pos);
+  myservo7.write(pos);
+  myservo8.write(pos);
+  delay(1000);
+   //leg 1 open:
+    for(pos = 40; pos <= 80; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo5.write(pos);
+    delay(30);
+  }
+  delay(d);
+  for(pos = 40; pos <= 80; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo1.write(pos);
+    delay(30);
+  }
+  delay(d);
+  for(pos = 80; pos >= 40; pos -= 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo5.write(pos);
+    delay(30);
+  }
+  delay(d);
+  // leg 1 close:
+}
+void loop()
+{
+  }
